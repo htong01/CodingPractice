56b433da5cb82e5085e8f4f0c878f805078b056f
Huilin <40194192+htong01@users.noreply.github.com>
2019-02-20 00:30:56 -0500

    Create Remove Nth Node From End of List.cpp

---
+++ LeetCode/LinkedList/Remove Nth Node From End of List.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* h1 = head, *h2 = head;
+        for(int i=0;i<n;i++) h1 = h1->next;
+        while(h1->next){
+            h1 = h1->next;
+            h2 = h2->next;
+        }
+        h2->next = h2->next->next;
+        return head;                  // return head????????
+    }
+};
