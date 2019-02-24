aa178b566e6b962056a3bb8c940f2e63312b5fc7
Huilin <40194192+htong01@users.noreply.github.com>
2019-02-24 15:00:12 -0500

    Update 973.K Closest Points to Origin.cpp

--- Amazon SDE OA/973.K Closest Points to Origin.cpp
+++ Amazon SDE OA/973.K Closest Points to Origin.cpp
@@ -6,3 +6,25 @@ The rest part of this problem equals to finding the K smallest elements.
   Constract a heap with only K element, and if the next element is smaller than the current largest number in the heap(heap.top()), then replace the largest number.
 */
 
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        priority_queue< vector<int>,vector<vector<int>>, compare > pq;
+        for(auto p:points){
+            pq.push(p);
+            if(pq.size()>K) pq.pop();
+        }
+        vector<vector<int>> ans;
+        while(!pq.empty()){
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        return ans;
+    }
+private:
+    struct compare{
+        bool operator()(const vector<int>& p, const vector<int>& q) {
+            return p[0] * p[0] + p[1] * p[1] < q[0] * q[0] + q[1] * q[1];
+        }
+    };
+};
