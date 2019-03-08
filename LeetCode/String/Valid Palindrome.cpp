08aac5b5c3f69591701377f3f3d4f215d8c94c4d
Huilin <40194192+htong01@users.noreply.github.com>
2019-03-08 14:54:03 -0500

    Create Valid Palindrome.cpp

---
+++ LeetCode/String/Valid Palindrome.cpp
@@ -0,0 +1,5 @@
+Similar to Reverse String question, two pointers on points at begining and one at end
+
+Some details in function:
+int isalnum(int x); whether entered character is alphanumeric, 去掉逗号空格之类
+tolower(); 统一小写
