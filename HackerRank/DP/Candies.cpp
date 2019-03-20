4be2ec9454c55a79acc52dea059d8f3ce336cc55
Huilin <40194192+htong01@users.noreply.github.com>
2019-03-20 11:18:20 -0400

    Update Candies.cpp

--- HackerRank/DP/Candies.cpp
+++ HackerRank/DP/Candies.cpp
@@ -1,31 +1,20 @@
-Go left to right and set "next" candy value to either "previous+1" or "1". Then go right to left and do the same. Very simple O(2xN) solution
-Passed only half test cases in this way.
+// Go left to right and set "next" candy value to either "previous+1" or "1". Then go right to left and do the same. Very simple O(2xN) solution
+// Passed only half test cases in this way.
+
+// Initialize vector<int> 要注意，这里一开始没写对
 
 long candies(int n, vector<int> arr) {
-    vector<int> t1;
-    t1.push_back(1);
-    vector<int> t2;
-    t2.push_back(1);
-
-    int temp = 1;
+    vector<int> candies(n,0);
+    candies[0] = 1;
     for(int i=1;i<n;i++){
-        if(arr[i]>arr[i-1]) temp += 1;
-        else temp = 1;
-        t1.push_back(temp);
+        candies[i] = (arr[i]>arr[i-1]) ? (candies[i-1]+1) : 1;
     }
-    int t = 1;
-    for(int j=n-1;j>=0;j--){
-        if(arr[j]>arr[j+1]) t += 1;
-        else t = 1;
-        t2.push_back(t);
+    for(int i=n-1;i>=0;i--){
+        candies[i] = (arr[i]>arr[i+1] && (arr[i+1]+1)>=arr[i]) ? (candies[i+1]+1) : candies[i];
     }
-    reverse(t2.begin(),t2.end());
     long ans = 0;
-    for(int k=0;k<n;k++){
-        ans += max(t1[k],t2[k]);
+    for(int i=0;i<n;i++){
+        ans += candies[i];
     }
     return ans;
 }
-
---------------------------------------------------------------------------------------------
-
