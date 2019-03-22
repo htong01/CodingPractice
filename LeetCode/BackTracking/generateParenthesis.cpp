0f6d3c8e54863a4f8b60e57ccd3e94f097de445a
Huilin <40194192+htong01@users.noreply.github.com>
2019-03-21 23:54:42 -0400

    Create generateParenthesis.cpp

---
+++ LeetCode/BackTracking/generateParenthesis.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        helper(res, "", n, 0);
+        return res;
+    }
+    
+    void helper(vector<string> &res, string str, int n, int m){
+        if(n==0 && m==0){
+            res.push_back(str);
+            return;
+        }
+        if(m>0) helper(res, str+')', n,  m-1);
+        if(n>0) helper(res, str+'(', n-1, m+1);
+    }
+};
