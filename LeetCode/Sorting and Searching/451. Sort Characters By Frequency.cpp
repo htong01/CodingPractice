7f83bb4183d02a648a836df84687600aed5a1368
Huilin <40194192+htong01@users.noreply.github.com>
2019-03-27 23:43:24 -0400

    Update 451. Sort Characters By Frequency.cpp

--- LeetCode/Sorting and Searching/451. Sort Characters By Frequency.cpp
+++ LeetCode/Sorting and Searching/451. Sort Characters By Frequency.cpp
@@ -1,2 +1,16 @@
-Top k 系列，hashmap
+// Top k 系列，hashmap
 
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int> map;
+        for(auto c:s) map[c]++;
+        
+        auto comp = [&map] (char a, char b){
+            return map[a]>map[b] || (map[a]==map[b] && a>b);
+        };
+        
+        sort(s.begin(),s.end(),comp);
+        return s;
+    }
+};
