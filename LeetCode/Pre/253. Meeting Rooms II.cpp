00e3f18a0349a137b0c19ce2ba58b8b56b406f4e
Huilin <40194192+htong01@users.noreply.github.com>
2019-03-31 16:49:33 -0400

    Create 253. Meeting Rooms II.cpp

---
+++ LeetCode/Pre/253. Meeting Rooms II.cpp
@@ -0,0 +1,18 @@
+Sort + min heap
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        if(intervals.empty()) return {};
+        
+        sort(intervals.begin(), intervals.end(), [](Interval a, Interval b){return a.start<b.start;});
+        priority_queue<int, vector<int>, greater<int> > min_heap;
+        
+        for(auto in:intervals){
+            if(!min_heap.empty() && min_heap.top()<=in.start)
+                min_heap.pop();
+            min_heap.push(in.end);
+        }
+        return min_heap.size();
+    }
+};
